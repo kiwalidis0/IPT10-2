a1b96f33f5563a8b5bd0974fe8129c2c245b4acf
Andreas <luy.andreas@auf.edu.ph>
2024-08-17 09:32:39 +0800

    Update index.php
    Added pagination and modified the php code to display the csv file.

--- labs-2425/lab2b/index.php
+++ labs-2425/lab2b/index.php
@@ -1,38 +1,40 @@
 <?php
 
-define('CUSTOMERS_FILE_PATH', 'customers-100.csv');
+$time_start = microtime(true);
 
-function get_hundred_customers_data()
+$csv = '/Users/kiwalidis0/IPT10/labs-2425/lab2b/customers-100000.csv';
+$customers = [];
+
+if (($handle = fopen($csv, 'r')) !== FALSE) 
 {
-    $opened_file_handler = fopen(CUSTOMERS_FILE_PATH, 'r');
+    fgetcsv($handle);
 
-    $data = [];
-    $headers = [];
-    $row_count = 0;
-    while (!feof($opened_file_handler)) {
-
-        $row = fgetcsv($opened_file_handler, 1024);
-        if (!empty($row)) {
-            if ($row_count == 0) {
-                array_push($headers, $row);    
-            } else {
-                array_push($data, $row);
-            }
-        }
-
-        $row_count++;
-
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $customers[] = $data;
     }
-
-    return [
-        'headers' => $headers,
-        'data' => $data
-    ];
+    fclose($handle);
 }
 
-$customers = get_hundred_customers_data();
+$results_per_page = 100; 
+$total_records = count($customers);
+$total_pages = ceil($total_records / $results_per_page);
+
+$page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+$page = ($page > $total_pages) ? $total_pages : $page;
+$page = ($page < 1) ? 1 : $page;
+
+$start_index = ($page - 1) * $results_per_page;
+
+$paginated_customers = array_slice($customers, $start_index, $results_per_page);
+
+$time_end = microtime(true);
+$execution_time = ($time_end - $time_start);
+
+echo "<br><strong>Execution Time:</strong> " . number_format($execution_time, 5) . " seconds";
+
 
 ?>
+
 <html>
 <head>
     <meta charset="utf-8">
@@ -65,11 +67,11 @@ The dataset is retrieved from this URL <a href="https://www.datablist.com/learn/
     </thead>
     <tbody>
     <?php
-    foreach ($customers['data'] as $record):
+    foreach ($paginated_customers as $record):
     ?>
         <tr>
             <td><?php echo $record[1]; ?></td>
-            <td><?php echo "<strong>{$record[3]}</strong>, {$record[2]}"; ?></td>
+            <td><?php echo "<strong>{$record[1]}</strong>, {$record[2]}"; ?></td>
             <td><?php echo $record[4]; ?></td>
             <td><?php echo $record[7]; ?></td>
             <td><?php echo $record[9]; ?></td>
@@ -80,6 +82,15 @@ The dataset is retrieved from this URL <a href="https://www.datablist.com/learn/
     </tbody>
 </table>
 
+<div>
+    <?php if ($page > 1): ?>
+        <a href="?page=<?php echo $page - 1; ?>">Previous</a>
+    <?php endif; ?>
+
+    <?php if ($page < $total_pages): ?>
+        <a href="?page=<?php echo $page + 1; ?>"><b>Next</b></a>
+    <?php endif; ?>
+</div>
 
 </body>
 </html>
